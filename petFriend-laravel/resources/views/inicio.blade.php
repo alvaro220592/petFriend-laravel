b413fe78e6267f216a32a74115b1ae245dc4efe0
alvaro220592 <alvaro220592@gmail.com>
2021-06-08 08:21:07 -0300

    ...

---
+++ petFriend-laravel/resources/views/inicio.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+@section('title', 'PetFriend - Início')
+@section('content')
+    <h1>Início</h1>
+@endsection
---
+++ petFriend-laravel/resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        {{--fontes do google--}}
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        {{--bootstrap--}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+        {{--css local--}}
+        <link rel="stylesheet" href="/css/style.css">
+
+    </head>
+    <body class="antialiased">
+        
+        @yield('content')
+        <footer>
+            <p>PetFriend &copy;</p>
+        </footer>
+    </body>
+</html>
