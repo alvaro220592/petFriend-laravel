7678453e41d7e8d76646a9407994fcf28403c7b8
alvaro220592 <alvaro220592@gmail.com>
2021-06-12 01:27:52 -0300

    ...

--- petFriend-laravel/resources/views/layouts/main.blade.php
+++ petFriend-laravel/resources/views/layouts/main.blade.php
@@ -7,8 +7,14 @@
         <title>@yield('title')</title>
 
         {{--fontes do google--}}
+        {{--Amatic SC--}}
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap" rel="stylesheet"> 
+
+        {{--Acme--}}
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Acme&family=Amatic+SC:wght@400;700&display=swap" rel="stylesheet"> 
+
 
         {{--bootstrap--}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
@@ -18,8 +24,38 @@
 
     </head>
     <body class="antialiased">
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <div class="logo-container">
+                            <img src="/img/dog-track.png" alt="" id="logo-img" class="mx-3 mt-3">
+                            <div id="title-subtitle-container">
+                                <h1 id="logo-title">PetFriend</h1>
+                                <span id="logo-subtitle">Para o seu melhor amigo</span>
+                            </div>
+                        </div>
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/events/create" class="nav-link">Criar Evento</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
         
         @yield('content')
+
         <footer>
             <p>PetFriend &copy;</p>
         </footer>
