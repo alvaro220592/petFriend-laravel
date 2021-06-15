4479a608da4d40237694e8e7a7b62629907e3ea8
alvaro220592 <alvaro220592@gmail.com>
2021-06-15 17:29:36 -0300

    ...

--- petFriend-laravel/resources/views/index.blade.php
+++ petFriend-laravel/resources/views/index.blade.php
@@ -1,5 +1,70 @@
 @extends('layouts.main')
 @section('title', 'PetFriend - Início')
 @section('content')
-    <h1>Início</h1>
+    <h1>Agendamento</h1>
+    <div class="container">
+        
+        <div class="row">
+            <div class="col-md-6 bg-warning h-50">
+                <form action="" method="post" class="">
+                    <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="nome" id="nome" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="nome" id="nome" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="nome" id="nome" class="form-control">
+                    </div>
+                </form>
+            </div>
+            <div class="agendamentos col-md-6">
+                <div class="">
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                </div>
+                <div class="">
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                </div>
+                <div class="">
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                </div>
+                <div class="">
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                </div>
+                <div class="">
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                </div>
+                <div class="">
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                </div>
+                <div class="">
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                    <p>Nome: hgjhgjhg</p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
