e2b6fcc96a97d07d2896a400b300116d5e96da39
alvaro220592 <alvaro220592@gmail.com>
2021-07-28 12:30:29 -0300

    ...

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -131,6 +131,6 @@
         {{-- ION ICONS --}}
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
+        <script src="js/scripts.js"></script>
     </body>
 </html>
