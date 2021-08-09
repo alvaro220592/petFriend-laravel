682e1d74d5a948ba48cbd63649d3d252a5d09a3a
alvaro220592 <alvaro220592@gmail.com>
2021-08-09 11:35:04 -0300

    ...

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,4 @@
-// $(function(){
-//     $('#botaoModal').trigger('click')
-// });
-
+// VIACEP
 const cep = document.querySelector("#cep")
 
 const showData = (result)=>{
@@ -27,4 +24,27 @@ cep.addEventListener("blur",(e)=>{
     .catch(e => console.log('Deu Erro: '+ e,message))
 })
 
-alert('a')
+// MODAL
+$(document).ready(function () {
+    $('#pet, #pet_label').hide(); 
+    $('#tutor').on('change', function () {
+        $('#pet, #pet_label').show(500);
+        {{--$('#tutor').animate({width:"50%"})--}}
+        let id = $(this).val();
+        $('#pet').empty();
+        $('#pet').append(`<option value="0" disabled selected>Processando...</option>`);
+        $.ajax({
+            type: 'GET',
+            url: 'getPets/' + id,
+            success: function (response) {
+                var response = JSON.parse(response);
+                console.log(response);   
+                $('#pet').empty();
+                $('#pet').append(`<option value="0" disabled selected>Selecione o pet</option>`);
+                response.forEach(element => {
+                    $('#pet').append(`<option value="${element['id']}">${element['pet_name']}</option>`);
+                });
+            }
+        });
+    });
+});
